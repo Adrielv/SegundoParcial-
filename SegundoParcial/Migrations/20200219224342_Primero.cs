dc41911dee1f4cd02a3cadc8a541140e92c56aab
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2020-02-19 18:06:28 -0500

    Termine

---
+++ SegundoParcial/Migrations/20200219224342_Primero.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SegundoParcial.Migrations
+{
+    public partial class Primero : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inscripciones",
+                columns: table => new
+                {
+                    InscripcionId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Semestre = table.Column<string>(nullable: false),
+                    Limite = table.Column<int>(nullable: false),
+                    Tomados = table.Column<int>(nullable: false),
+                    Disponibles = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inscripciones", x => x.InscripcionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inscripciones");
+        }
+    }
+}
