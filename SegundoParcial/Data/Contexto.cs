1a286d687da17c85e23e6c41ce11975d6c96d967
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2020-02-19 16:24:46 -0500

    Listo

---
+++ SegundoParcial/Data/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcial.Data
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite(@"Data source = Database\ArticulosDataa.db");
+        }
+    }
+}
