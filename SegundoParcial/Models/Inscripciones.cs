812458cb04843580e0d170dc892ea2a2b6173d31
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2020-02-19 17:05:24 -0500

    Listo

---
+++ SegundoParcial/Models/Inscripciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcial.Models
+{
+    public class Inscripciones
+    {
+        [Key]
+
+        public int InscripcionId { get; set; }
+
+        [Required(ErrorMessage ="No puede estar vacio")]        
+        public string Semestre { get; set; }
+        [Required(ErrorMessage = "No puede estar vacio")]
+        [Range(minimum: 1, maximum: 10000, ErrorMessage = "No puede pasar el limite")]
+        public int Limite { get; set; }
+        [Required(ErrorMessage = "No puede estar vacio")]
+        [Range(minimum: 1, maximum: 10000, ErrorMessage = "No puede pasar el limite")]
+        public int Tomados { get; set; }
+        [Required(ErrorMessage = "No puede estar vacio")]
+        [Range(minimum: 1, maximum: 10000, ErrorMessage = "No puede pasar el limite")]
+        public int Disponibles { get; set; }
+
+        public Inscripciones()
+        {
+            InscripcionId = 0;
+            Semestre = string.Empty;
+            Limite = 0;
+            Tomados = 0;
+            Disponibles = 0;
+        }
+    }
+}
